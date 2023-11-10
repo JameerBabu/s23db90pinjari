8f14666afb51ee2d78a6c57721c92f2509e661b3
Jameer Babu <jameer.jb@gmail.com>
2023-11-10 10:05:44 -0600

    lab 2 screeshot

--- controllers/van.js
+++ controllers/van.js
@@ -46,7 +46,7 @@ exports.van_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"van_type":"goat", "cost":12, "size":"large"}
+    // {"Company":"goat", "cost":12, "model":"large"}
     document.Company = req.body.Company;
     document.Model = req.body.Model;
     document.cost = req.body.cost;
@@ -66,9 +66,31 @@ exports.van_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: van delete DELETE ' + req.params.id);
 };
 // Handle van update form on PUT.
-exports.van_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: van update PUT' + req.params.id);
-};
+// exports.van_update_put = function(req, res) {
+// res.send('NOT IMPLEMENTED: van update PUT' + req.params.id);
+// };
+
+
+//Handle van update form on PUT.
+exports.van_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await van.findById( req.params.id)
+// Do updates of properties
+if(req.body.Company)
+toUpdate.Company = req.body.Company;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.Model) toUpdate.Model = req.body.Model;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+}
 
 // VIEWS
 // Handle a show all view
