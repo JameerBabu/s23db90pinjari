ff8491467db19597adf6f9ddda80b06b7951147b
Jameer Babu <jameer.jb@gmail.com>
2023-11-10 09:48:02 -0600

    lab 1 screeshot

--- controllers/van.js
+++ controllers/van.js
@@ -18,9 +18,22 @@ exports.van_list = async function(req, res) {
 };
 
 // for a specific van.
-exports.van_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: van detail: ' + req.params.id);
-};
+// exports.van_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: van detail: ' + req.params.id);
+// };
+
+exports.van_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await van.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+
 // Handle van create on POST.
 // exports.van_create_post = function(req, res) {
 // res.send('NOT IMPLEMENTED: van create POST');
