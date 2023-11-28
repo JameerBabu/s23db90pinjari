3bb04992bfeec1b13d11a059d0a8bbdeb9e030f1
Jameer Babu <jameer.jb@gmail.com>
2023-11-28 14:49:47 -0600

    screeshot 5

--- routes/vans.js
+++ routes/vans.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const van_controlers= require('../controllers/van');
 var router = express.Router();
 /* GET vans */
@@ -8,7 +9,20 @@ router.get('/detail', van_controlers.van_view_one_Page);
 /* GET create van page */
 router.get('/create', van_controlers.van_create_Page);
 /* GET create update page */
-router.get('/update', van_controlers.van_update_Page);
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/update', secured,van_controlers.van_update_Page);
 /* GET delete van page */
 router.get('/delete', van_controlers.van_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    
 module.exports = router;
